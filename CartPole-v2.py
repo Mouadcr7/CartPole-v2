847fcc17bced6988462f3daf874923967cbec52d
Mouadcr7 <mouad.ble@gmail.com>
2023-03-02 18:31:15 +0100

    Task 2: DQL_Agent -  first attempt.

--- CartPole-v2.py
+++ CartPole-v2.py
@@ -1,11 +1,15 @@
 import gym
 import numpy as np
+import torch
 import math
 import time
+import random
+import copy
 from typing import Optional
 from gym import logger 
 from gym.utils.env_checker import check_env
 from gym.envs.classic_control import CartPoleEnv
+from collections import deque
 
 
 
@@ -13,6 +17,7 @@ from gym.envs.classic_control import CartPoleEnv
 class Random_agent:
     def __init__(self, env):
         self.env = env
+        self.env.reset()
         pass
 
     def policy(self) :
@@ -23,25 +28,137 @@ class Random_agent:
         return self.env.action_space.sample()
     
 
+class DQL_agent :
+    def __init__(self, env, hidden_dim=64, lr=0.05):
+        self.env = env
+        self.env.reset()
+        # parameters and hyperparameters
+        self.state_size = env.observation_space.shape[0] # this is for input of neural network node size
+        self.action_size = env.action_space.n # this is for out of neural network node size
+        
+        self.loss_fn = torch.nn.MSELoss()
+        self.model = torch.nn.Sequential(
+                            torch.nn.Linear(self.state_size, hidden_dim),
+                            torch.nn.LeakyReLU(),
+                            torch.nn.Linear(hidden_dim, hidden_dim*2),
+                            torch.nn.LeakyReLU(),
+                            torch.nn.Linear(hidden_dim*2,self.action_size)
+                    )
+        self.target = copy.deepcopy(self.model)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr)
+
+        self.train(epochs = 200, mem_size = 30, batch_size = 20, sync_freq = 20)
+
+
+
+    
+    def update(self, state, y):
+        """Update the weights of the network given a training sample. """
+        y_pred = self.model(torch.Tensor(np.array(state)))
+        loss = self.loss_fn(y_pred, (torch.Tensor(np.array(y))))
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def predict(self, state):
+        """ Compute Q values for all actions using the DQL. """
+        with torch.no_grad():
+            return self.model(torch.Tensor(state))
+        
+    def target_predict(self, s):
+        ''' Use target network to make predicitons.'''
+        print(s)
+        with torch.no_grad(): 
+            return self.target(torch.Tensor(s))
+        
+    def target_update(self):
+        ''' Update target network with the model weights.'''
+        self.target.load_state_dict(self.model.state_dict())
+        
+    def replay(self, memory, size, gamma=1.0):
+        ''' Add experience replay to the DQL network class.'''
+        if len(memory) > size:
+            # Sample experiences from the agent's memory
+            data = random.sample(memory, size)
+            states = []
+            targets = []
+            # Extract datapoints from the data
+            for state, action, reward, next_state, done in data:
+                states.append(state)
+                q_values = self.predict(state).tolist()
+                if done:
+                    q_values[action] = reward
+                else:
+                    # The only difference between the simple replay is in this line
+                    # It ensures that next q values are predicted with the target network.
+                    q_values_next = self.target_predict(next_state)
+                    q_values[action] = reward + gamma * torch.max(q_values_next).item()
+            targets.append(q_values)
+            self.update(states, targets)
+
+    def train(self, epochs = 5000, mem_size = 1000, batch_size = 200, sync_freq = 500 ) :
+        ''' '''
+        counter = 0
+        epsilon = 0.3
+        eps_decay=0.99
+        memory = deque([],maxlen=mem_size)
+        for i in range(epochs):
+            counter += 1
+            current_state = self.env.reset()[0]
+            current_state_ = torch.from_numpy(current_state).float()
+            while True :
+                qval = self.predict(current_state_)
+                qval_ = qval.data.numpy()
+                action = 0 
+                if (random.random() < epsilon):
+                    action = np.random.randint(0,self.action_size)
+                else:
+                    action = np.argmax(qval_)
+                next_state, reward, terminated, truncated, info = self.env.step(action)
+                next_state_ = torch.from_numpy(next_state).float()
+                exp =  (current_state, action, reward, next_state, terminated or truncated)
+                memory.append(exp)
+                self.replay(memory, batch_size, gamma=1.0)
+                current_state = next_state
+                current_state_ = next_state_
+                if sync_freq % counter == 0 :
+                    self.target_update()
+                if terminated or truncated :
+                    self.env.close()
+                    break
+            
+
+    def policy(self, state) :
+            '''
+            Define the policy of the agent, as a DQL agent it selects "the best action ".
+            Ouptut : an int corresponding to the selected action : 0 left , 1 right, 2 do nothing
+            '''
+            return np.argmax(self.predict(state).data.numpy())
+
+
+
+
+    
+
 class Simu:
     def __init__(self, render_mode="human", agent=None, max_steps=None):
         self.env = CartPole_v2(render_mode)
         self.obs = self.env.reset()
-        self.agent = Random_agent(self.env) if agent is None else agent
+        self.agent = Random_agent(CartPole_v2()) if agent is None else agent(CartPole_v2())
         self.max_steps = 500 if max_steps is None or max_steps > 500 else max_steps
         self.step_count = 0 
 
-    def step(self):
-        action = self.agent.policy() 
+    def step(self,state):
+        action = self.agent.policy(state) 
         return self.env.step(action)
     
     def run_simu(self):
         while self.step_count < self.max_steps :
             time.sleep(0.2)
             self.env.render()
-            self.obs, reward, terminated, truncated, info = self.step()
+            self.obs, reward, terminated, truncated, info = self.step(self.obs)
             self.step_count += 1
-            if terminated :
+            if terminated or truncated :
                 self.env.close()
                 return
         
@@ -117,6 +234,7 @@ class CartPole_v2(CartPoleEnv):
 
 
 
-sm = Simu()
+sm = Simu(agent=DQL_agent)
+
 sm.run_simu()
 print(sm.step_count)
